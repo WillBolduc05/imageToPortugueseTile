ace522dd4fa87f90c1393ff51f234dfe1ad5b2bd
William Bolduc <119345704+WillBolduc05@users.noreply.github.com>
2023-05-22 19:40:32 -0500

    Re-added args

--- sketchConverter.py
+++ sketchConverter.py
@@ -2,13 +2,12 @@ import cv2
 import os
 from PIL import Image, ImageOps
 #this function was based on the one found here: https://towardsdatascience.com/generate-pencil-sketch-from-photo-in-python-7c56802d8acb
-def convertToSketch(photo, k_size, greyName, resultName):
+def convertToSketch(photo, k_size):
     #Opening up the image that we were passed
     img=cv2.imread(photo)
     #Converting the image into greyscale
     imgGrey=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #saving the grey image (will be used later)
-    cv2.imwrite(greyName, imgGrey)
     #Inverting the image and blurring it using gaussian blur
     imgInvt=cv2.bitwise_not(imgGrey)
     imgBlur=cv2.GaussianBlur(imgInvt, (k_size,k_size),0)
@@ -16,18 +15,26 @@ def convertToSketch(photo, k_size, greyName, resultName):
     imgBlurInvt=cv2.bitwise_not(imgBlur)
     #deriving the "pencil sketch" image by dividing the grey image by the blurred one
     imgSketch=cv2.divide(imgGrey,imgBlurInvt, scale=256.0)
-    
-    # Saving the sketch image
-    cv2.imwrite(resultName, imgSketch)
+    return imgSketch
     
 #this function alters the coloring of the image using PIL's colorize method
-def blueScale(photo):
+def blueScaleFromGrey(photo):
     #opening up the sketch image
-    img =  Image.open(photo, "r")
+    img = Image.fromarray(photo)
     #utilizing PIL's colorize to change to bluescale
     img = ImageOps.colorize(img, black = "#4c9ccc", white = "#e9ddb3")
     #img.show()
     return img
+
+def blueScaleFromRGB(photo):
+    #opening up the sketch image
+    imgGrey=cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
+    img = Image.fromarray(imgGrey)
+    #utilizing PIL's colorize to change to bluescale
+    img = ImageOps.colorize(img, black = "#4c9ccc", white = "#e9ddb3")
+    #img.show()
+    return img
+
 def imgOverlay(photo, overlay, result):
     #opening up the images
     img1 = bPhoto
@@ -38,10 +45,15 @@ def imgOverlay(photo, overlay, result):
     img1.save(result)
     img1.show()
 
-blueSketch = convertToSketch(photo='plains.jpg', k_size=(int(input("What thickness would you like, on a scale of 1-10?\n")) * 20 + 1), greyName = 'grey.png', resultName = 'sketch.png')
-bSketch = blueScale(photo='sketch.png')
-bPhoto = blueScale(photo='grey.png')
+ARGPARSER = argparse.ArgumentParser()
+ARGPARSER.add_argument("--thickness", type=int)
+ARGPARSER.add_argument("--input", type=str)
+ARGPARSER.add_argument("--output", type=str)
+ARGS = ARGPARSER.parse_args()
 
-imgOverlay(photo=bSketch, overlay = bPhoto, result = 'result1.png')
-os.remove("sketch.png")
-os.remove("grey.png")
+blueSketch = convertToSketch(photo=ARGS.input, k_size=(ARGS.thickness * 20 + 1))
+bSketch = blueScaleFromGrey(photo=blueSketch)
+original = cv2.imread('plains.jpg')
+bPhoto = blueScaleFromRGB(photo=original)
+
+imgOverlay(photo=bSketch, overlay = bPhoto, result = ARGS.output)
