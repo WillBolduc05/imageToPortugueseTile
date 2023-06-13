63c33ec89efa49553c19da403bdda9900924bfa9
William Bolduc <119345704+WillBolduc05@users.noreply.github.com>
2023-06-13 18:47:14 -0500

    Implemented actually showing the user the image

--- sketchConverter.py
+++ sketchConverter.py
@@ -4,6 +4,8 @@ import cv2
 import os
 import argparse
 import numpy
+import base64
+import io
 from PIL import Image, ImageOps
 
 def convertToSketch(photo, k_size):
@@ -65,6 +67,7 @@ def drawTiles(photo, xLines, yLines, thickness, result):
         startPoint = (int(0),int(y))
         endPoint = (int(w),int(y))
         cv2.line(cvPhoto, startPoint, endPoint,color,thickness)
+    #REMOVE THIS ONCE IT IS NO LONGER NEEDED FOR TESTING :)
     cv2.imwrite(result,cvPhoto)
     return cvPhoto
 
@@ -76,7 +79,9 @@ def convertImage(photo, thickness = 1, xTiles = 4, yTiles = 4, tileThickness = 2
         bPhoto = blueScaleFromRGB(photo=img1)
         overlayPhoto = imgOverlay(photo=bSketch, overlay = bPhoto)
         output = drawTiles(photo=overlayPhoto, xLines = xTiles, yLines = yTiles, thickness = tileThickness, result = "output.png")
-        return output
+        outputStep1 = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+        PILOutput = Image.fromarray(outputStep1)
+        return PILOutput
 
 
 
@@ -85,16 +90,18 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        return render_template('index.html', msg='')
+        return render_template('index.html')
 
     image = request.files['file']
     img = Image.open(image)
     img = numpy.array(img)
     
-    print(img)
-    print(img.shape)
     output = convertImage(photo = img, thickness = 1, xTiles = 4, yTiles = 4, tileThickness = 2)
-    return render_template('index.html', msg='Here is the tile version of your image! Enjoy!')
+    data = io.BytesIO()
+    output.save(data, "JPEG")
+    encoded_img_data = base64.b64encode(data.getvalue())
+
+    return render_template('image.html', msg='Here is the tile version of your image! Enjoy!', img_data=encoded_img_data.decode('utf-8'))
 
 if __name__ == '__main__':
     app.run()
